ca5e033942940d9620af8327cca9aabe994e2cdb
= <=>
2025-08-16 19:08:14 +0200

    added testing

--- java_backend/src/test/java/de/gras/java_backend/API/controllers/Items.java
+++ java_backend/src/test/java/de/gras/java_backend/API/controllers/Items.java
@@ -1,4 +1,4 @@
-package de.gras.java_backend.API;
+package de.gras.java_backend.API.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
